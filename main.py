af8e1148fdd2695986e5d47f408a393f17ec72e8
fherbine <felix.herbinet@yahoo.com>
2019-03-09 14:05:22 +0100

    Implementing app.time + app.screenmanager

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from kivy.app import App
+from kivy.clock import Clock
+from kivy.properties import ObjectProperty
+
 import config
 
 
 class FappyBirdApp(App):
+    screenmanager = ObjectProperty()
+
     def build(self):
-        pass
+        self.time = 0.0
+        Clock.schedule_interval(self.update_time, 0)
+
+    def update_time(self, dt)
+        self.time += dt
 
 if __name__ == '__main__':
     FappyBirdApp().run()
